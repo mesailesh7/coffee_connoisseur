9c3230c1b335ed969ac4ec26cc787c2f9d40b22e
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2022-04-25 17:48:55 -0600

    font update

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,33 @@
 import Document, { Head, Html, Main, NextScript } from "next/document";
 
+
+
+
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Bold.ttf"
+            as="font"
+            crossOrigin="anonymus"
+          ></link>
+          <link
+            rel="preload"
+            href="/fonts/Poppins-Regular.ttf"
+            as="font"
+            crossOrigin="anonymus"
+          ></link>
+          <link
+            rel="preload"
+            href="/fonts/Poppins-SemiBold.ttf"
+            as="font"
+            crossOrigin="anonymus"
+          ></link>
+
+        </Head>
         <body>
           <Main></Main>
           <NextScript />
